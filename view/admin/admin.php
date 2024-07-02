0cf936df518e7605c2b735f3c324e2e4ded37b11
Isracraxcker <andyisraeljaramillo@gmail.com>
2024-07-02 10:52:41 -0500

    modified:   view/admin/admin.php

--- view/admin/admin.php
+++ view/admin/admin.php
@@ -1,26 +1,32 @@
 <?php
-     session_start();
-    
-    if(!isset($_SESSION['cargo']) == 1){
-    header('location: ../../login.php');
-  }
+session_start();
+
+if (!isset($_SESSION['cargo']) == 1) {
+	header('location: ../../login.php');
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title>CITAS MEDICAS PANEL ADMINISTRATIVO</title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	<link rel="icon" href="../../assets/img/logo.ico" type="image/x-icon"/>
+	<link rel="icon" href="../../assets/img/logo.ico" type="image/x-icon" />
 
 	<!-- Fonts and icons -->
 	<script src="../../assets/js/plugin/webfont/webfont.min.js"></script>
 	<script>
 		WebFont.load({
-			google: {"families":["Lato:300,400,700,900"]},
-			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['../../assets/css/fonts.min.css']},
+			google: {
+				"families": ["Lato:300,400,700,900"]
+			},
+			custom: {
+				"families": ["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"],
+				urls: ['../../assets/css/fonts.min.css']
+			},
 			active: function() {
 				sessionStorage.fonts = true;
 			}
@@ -34,13 +40,14 @@
 	<!-- CSS Just for demo purpose, don't include it in your project -->
 	<link rel="stylesheet" href="../../assets/css/demo.css">
 </head>
+
 <body>
 
 	<div class="wrapper">
 		<div class="main-header">
 			<!-- Logo Header -->
 			<div class="logo-header" data-background-color="blue">
-				
+
 				<a href="admin.php" class="logo">
 					<img src="../../assets/img/logo.svg" alt="navbar brand" class="navbar-brand">
 				</a>
@@ -60,7 +67,7 @@
 
 			<!-- Navbar Header -->
 			<nav class="navbar navbar-header navbar-expand-lg" data-background-color="blue2">
-				
+
 				<div class="container-fluid">
 					<div class="collapse" id="search-nav">
 						<form class="navbar-left navbar-form nav-search mr-md-3">
@@ -80,44 +87,9 @@
 								<i class="fa fa-search"></i>
 							</a>
 						</li>
-						<li class="nav-item dropdown hidden-caret">
-							<a class="nav-link dropdown-toggle" href="#" id="messageDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<i class="fa fa-envelope"></i>
-							</a>
-							<ul class="dropdown-menu messages-notif-box animated fadeIn" aria-labelledby="messageDropdown">
-								<li>
-									<div class="dropdown-title d-flex justify-content-between align-items-center">
-										Messages 									
-										<a href="#" class="small">Mark all as read</a>
-									</div>
-								</li>
-								<li>
-									
-								</li>
-								<li>
-									<a class="see-all" href="javascript:void(0);">See all messages<i class="fa fa-angle-right"></i> </a>
-								</li>
-							</ul>
-						</li>
-						<li class="nav-item dropdown hidden-caret">
-							<a class="nav-link dropdown-toggle" href="#" id="notifDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<i class="fa fa-bell"></i>
-								<span class="notification">0</span>
-							</a>
-							<ul class="dropdown-menu notif-box animated fadeIn" aria-labelledby="notifDropdown">
-								<li>
-									<div class="dropdown-title">You have 0 new notification</div>
-								</li>
-								<li>
-								
-									
-								</li>
-								<li>
-									<a class="see-all" href="javascript:void(0);">See all notifications<i class="fa fa-angle-right"></i> </a>
-								</li>
-							</ul>
-						</li>
-						
+
+
+
 						<li class="nav-item dropdown hidden-caret">
 							<a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
 								<div class="avatar-sm">
@@ -128,7 +100,7 @@
 								<div class="dropdown-user-scroll scrollbar-outer">
 									<li>
 										<div class="user-box">
-										
+
 											<div class="avatar-lg"><img src="../../assets/img/avatar.png" alt="image profile" class="avatar-img rounded"></div>
 											<div class="u-text">
 												<h4><?php echo ucfirst($_SESSION['nombre']); ?></h4>
@@ -139,7 +111,7 @@
 									<li>
 										<div class="dropdown-divider"></div>
 										<a class="dropdown-item" href="#">Mi Perfil</a>
-										
+
 										<div class="dropdown-divider"></div>
 										<a class="dropdown-item" href="../../cerrarSesion.php">Cerar Sesion</a>
 									</li>
@@ -153,7 +125,7 @@
 		</div>
 
 		<!-- Sidebar -->
-		<div class="sidebar sidebar-style-2">			
+		<div class="sidebar sidebar-style-2">
 			<div class="sidebar-wrapper scrollbar scrollbar-inner">
 				<div class="sidebar-content">
 					<div class="user">
@@ -198,7 +170,7 @@
 								<p>Home</p>
 								<span class="caret"></span>
 							</a>
-							
+
 						<li class="nav-item">
 							<a data-toggle="collapse" href="#base">
 								<i class="fas fa-layer-group"></i>
@@ -207,13 +179,13 @@
 							</a>
 							<div class="collapse" id="base">
 								<ul class="nav nav-collapse">
-									
+
 									<li>
 										<a href="../../folder/appointment.php">
 											<span class="sub-item">Mostrar</span>
 										</a>
 									</li>
-									
+
 								</ul>
 							</div>
 						</li>
@@ -230,7 +202,7 @@
 											<span class="sub-item">Mostrar</span>
 										</a>
 									</li>
-									
+
 								</ul>
 							</div>
 						</li>
@@ -263,7 +235,7 @@
 											<span class="sub-item">Mostrar</span>
 										</a>
 									</li>
-									
+
 								</ul>
 							</div>
 						</li>
@@ -282,7 +254,7 @@
 											<span class="sub-item">Mostrar</span>
 										</a>
 									</li>
-									
+
 								</ul>
 							</div>
 						</li>
@@ -301,11 +273,11 @@
 											<span class="sub-item">Mostrar</span>
 										</a>
 									</li>
-									
+
 								</ul>
 							</div>
 						</li>
-						
+
 					</ul>
 				</div>
 			</div>
@@ -319,9 +291,9 @@
 						<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
 							<div>
 								<h2 class="text-white pb-2 fw-bold">Dashboard</h2>
-								
+
 							</div>
-							
+
 						</div>
 					</div>
 				</div>
@@ -331,36 +303,36 @@
 							<div class="card full-height">
 								<div class="card-body">
 									<div class="card-title">Resumen</div>
-									
+
 									<div class="d-flex flex-wrap justify-content-around pb-2 pt-4">
 										<div class="px-2 pb-2 pb-md-0 text-center">
 											<div id="circles-1"></div>
 											<h6 class="fw-bold mt-3 mb-0">Pacientes</h6>
 											<?php
-											  require_once "../config/conexion1.php";
-											  $sql = "SELECT COUNT(*) total FROM customers";
-											  $result = $bd->query($sql); //$pdo sería el objeto conexión
-											  $total = $result->fetchColumn();
+											require_once "../config/conexion1.php";
+											$sql = "SELECT COUNT(*) total FROM customers";
+											$result = $bd->query($sql); //$pdo sería el objeto conexión
+											$total = $result->fetchColumn();
 											?>
 										</div>
 										<div class="px-2 pb-2 pb-md-0 text-center">
 											<div id="circles-2"></div>
 											<h6 class="fw-bold mt-3 mb-0">Doctores</h6>
 											<?php
-											  require_once "../config/conexion1.php";
-											  $sql = "SELECT COUNT(*) total FROM doctor";
-											  $result = $bd->query($sql); //$pdo sería el objeto conexión
-											  $total2 = $result->fetchColumn();
+											require_once "../config/conexion1.php";
+											$sql = "SELECT COUNT(*) total FROM doctor";
+											$result = $bd->query($sql); //$pdo sería el objeto conexión
+											$total2 = $result->fetchColumn();
 											?>
 										</div>
 										<div class="px-2 pb-2 pb-md-0 text-center">
 											<div id="circles-3"></div>
 											<h6 class="fw-bold mt-3 mb-0">Citas</h6>
 											<?php
-											  require_once "../config/conexion1.php";
-											  $sql = "SELECT COUNT(*) total FROM appointment";
-											  $result = $bd->query($sql); //$pdo sería el objeto conexión
-											  $total3 = $result->fetchColumn();
+											require_once "../config/conexion1.php";
+											$sql = "SELECT COUNT(*) total FROM appointment";
+											$result = $bd->query($sql); //$pdo sería el objeto conexión
+											$total3 = $result->fetchColumn();
 											?>
 										</div>
 									</div>
@@ -368,7 +340,7 @@
 							</div>
 						</div>
 
-						
+
 					</div>
 					<div class="row">
 						<div class="col-md-4">
@@ -376,41 +348,42 @@
 								<div class="card-body">
 									<div class="card-title fw-mediumbold">Nuevos pacientes</div>
 									<?php
-function connect(){
-return new mysqli("localhost","root","","proyecto_final");
-}
-$con = connect();
-$sql = "SELECT * FROM customers   ORDER BY apellidop ASC LIMIT 5";
-$query  =$con->query($sql);
-$data =  array();
-if($query){
-    while($r = $query->fetch_object()){
-        $data[] = $r;
-    }
-}
-?>
-<?php if(count($data)>0):?>
-	<?php foreach($data as $d):?>
-									<div class="card-list">
-<!-- POR ACA COMENTO O BAJO DATOS DE MYSQL PARA VISUALIZARLO CON LAS CONEXIONES--------  -->
+									function connect()
+									{
+										return new mysqli("localhost", "root", "", "proyecto_final");
+									}
+									$con = connect();
+									$sql = "SELECT * FROM customers   ORDER BY apellidop ASC LIMIT 5";
+									$query  = $con->query($sql);
+									$data =  array();
+									if ($query) {
+										while ($r = $query->fetch_object()) {
+											$data[] = $r;
+										}
+									}
+									?>
+									<?php if (count($data) > 0) : ?>
+										<?php foreach ($data as $d) : ?>
+											<div class="card-list">
+												<!-- POR ACA COMENTO O BAJO DATOS DE MYSQL PARA VISUALIZARLO CON LAS CONEXIONES--------  -->
 
-										<div class="item-list">
-											<div class="avatar">
-												<img src="../../assets/img/avatar.png" alt="..." class="avatar-img rounded-circle">
+												<div class="item-list">
+													<div class="avatar">
+														<img src="../../assets/img/avatar.png" alt="..." class="avatar-img rounded-circle">
+													</div>
+													<div class="info-user ml-3">
+														<div class="username"><?php echo $d->apellidop; ?></div>
+														<div class="status">Pacientes</div>
+													</div>
+
+												</div>
+
+
 											</div>
-											<div class="info-user ml-3">
-												<div class="username"><?php echo $d->apellidop; ?></div>
-												<div class="status">Pacientes</div>
-											</div>
-											
-										</div>
-
-
-									</div>
-									<?php endforeach; ?>
-                            <?php else:?>
-                            <p class="alert alert-warning"><strong>No hay datos</strong></p>
-                            <?php endif; ?> 
+										<?php endforeach; ?>
+									<?php else : ?>
+										<p class="alert alert-warning"><strong>No hay datos</strong></p>
+									<?php endif; ?>
 								</div>
 							</div>
 						</div>
@@ -421,38 +394,38 @@ if($query){
 									<div class="card-title fw-mediumbold">Nuevos doctores</div>
 									<?php
 
-$con = connect();
-$sql = "SELECT * FROM doctor   ORDER BY apedoc ASC LIMIT 5";
-$query  =$con->query($sql);
-$data =  array();
-if($query){
-    while($r = $query->fetch_object()){
-        $data[] = $r;
-    }
-}
-?>
-<?php if(count($data)>0):?>
-	<?php foreach($data as $d):?>
-									<div class="card-list">
-<!-- POR ACA COMENTO O BAJO DATOS DE MYSQL PARA VISUALIZARLO CON LAS CONEXIONES--------  -->
+									$con = connect();
+									$sql = "SELECT * FROM doctor   ORDER BY apedoc ASC LIMIT 5";
+									$query  = $con->query($sql);
+									$data =  array();
+									if ($query) {
+										while ($r = $query->fetch_object()) {
+											$data[] = $r;
+										}
+									}
+									?>
+									<?php if (count($data) > 0) : ?>
+										<?php foreach ($data as $d) : ?>
+											<div class="card-list">
+												<!-- POR ACA COMENTO O BAJO DATOS DE MYSQL PARA VISUALIZARLO CON LAS CONEXIONES--------  -->
 
-										<div class="item-list">
-											<div class="avatar">
-												<img src="../../assets/img/avatar.png" alt="..." class="avatar-img rounded-circle">
+												<div class="item-list">
+													<div class="avatar">
+														<img src="../../assets/img/avatar.png" alt="..." class="avatar-img rounded-circle">
+													</div>
+													<div class="info-user ml-3">
+														<div class="username"><?php echo $d->apedoc; ?></div>
+														<div class="status">Doctores</div>
+													</div>
+
+												</div>
+
+
 											</div>
-											<div class="info-user ml-3">
-												<div class="username"><?php echo $d->apedoc; ?></div>
-												<div class="status">Doctores</div>
-											</div>
-											
-										</div>
-
-
-									</div>
-									<?php endforeach; ?>
-                            <?php else:?>
-                            <p class="alert alert-warning"><strong>No hay datos</strong></p>
-                            <?php endif; ?> 
+										<?php endforeach; ?>
+									<?php else : ?>
+										<p class="alert alert-warning"><strong>No hay datos</strong></p>
+									<?php endif; ?>
 								</div>
 							</div>
 						</div>
@@ -463,49 +436,49 @@ if($query){
 									<div class="card-title fw-mediumbold">Nuevos especialidades</div>
 									<?php
 
-$con = connect();
-$sql = "SELECT * FROM specialty   ORDER BY nombrees ASC LIMIT 5";
-$query  =$con->query($sql);
-$data =  array();
-if($query){
-    while($r = $query->fetch_object()){
-        $data[] = $r;
-    }
-}
-?>
-<?php if(count($data)>0):?>
-	<?php foreach($data as $d):?>
-									<div class="card-list">
-<!-- POR ACA COMENTO O BAJO DATOS DE MYSQL PARA VISUALIZARLO CON LAS CONEXIONES--------  -->
+									$con = connect();
+									$sql = "SELECT * FROM specialty   ORDER BY nombrees ASC LIMIT 5";
+									$query  = $con->query($sql);
+									$data =  array();
+									if ($query) {
+										while ($r = $query->fetch_object()) {
+											$data[] = $r;
+										}
+									}
+									?>
+									<?php if (count($data) > 0) : ?>
+										<?php foreach ($data as $d) : ?>
+											<div class="card-list">
+												<!-- POR ACA COMENTO O BAJO DATOS DE MYSQL PARA VISUALIZARLO CON LAS CONEXIONES--------  -->
 
-										<div class="item-list">
-											
-											<div class="info-user ml-3">
-												<div class="username"><?php echo $d->nombrees; ?></div>
-												<div class="status">Especialidades</div>
+												<div class="item-list">
+
+													<div class="info-user ml-3">
+														<div class="username"><?php echo $d->nombrees; ?></div>
+														<div class="status">Especialidades</div>
+													</div>
+
+												</div>
+
+
 											</div>
-											
-										</div>
-
-
-									</div>
-									<?php endforeach; ?>
-                            <?php else:?>
-                            <p class="alert alert-warning"><strong>No hay datos</strong></p>
-                            <?php endif; ?> 
+										<?php endforeach; ?>
+									<?php else : ?>
+										<p class="alert alert-warning"><strong>No hay datos</strong></p>
+									<?php endif; ?>
 								</div>
 							</div>
 						</div>
-						
+
 					</div>
-				
+
 				</div>
 			</div>
-			
+
 		</div>
-		
-	
-		
+
+
+
 		<!-- End Custom template -->
 	</div>
 	<!--   Core JS Files   -->
@@ -534,7 +507,7 @@ if($query){
 	<script src="../../assets/js/plugin/datatables/datatables.min.js"></script>
 
 	<!-- Bootstrap Notify -->
-	
+
 
 	<!-- jQuery Vector Maps -->
 	<script src="../../assets/js/plugin/jqvmap/jquery.vmap.min.js"></script>
@@ -551,48 +524,48 @@ if($query){
 	<script src="../../assets/js/demo.js"></script>
 	<script>
 		Circles.create({
-			id:'circles-1',
-			radius:45,
-			value:60,
-			maxValue:100,
-			width:7,
+			id: 'circles-1',
+			radius: 45,
+			value: 60,
+			maxValue: 100,
+			width: 7,
 			text: <?php echo  $total; ?>,
-			colors:['#f1f1f1', '#FF9E27'],
-			duration:400,
-			wrpClass:'circles-wrp',
-			textClass:'circles-text',
-			styleWrapper:true,
-			styleText:true
+			colors: ['#f1f1f1', '#FF9E27'],
+			duration: 400,
+			wrpClass: 'circles-wrp',
+			textClass: 'circles-text',
+			styleWrapper: true,
+			styleText: true
 		})
 
 		Circles.create({
-			id:'circles-2',
-			radius:45,
-			value:70,
-			maxValue:100,
-			width:7,
+			id: 'circles-2',
+			radius: 45,
+			value: 70,
+			maxValue: 100,
+			width: 7,
 			text: <?php echo  $total2; ?>,
-			colors:['#f1f1f1', '#2BB930'],
-			duration:400,
-			wrpClass:'circles-wrp',
-			textClass:'circles-text',
-			styleWrapper:true,
-			styleText:true
+			colors: ['#f1f1f1', '#2BB930'],
+			duration: 400,
+			wrpClass: 'circles-wrp',
+			textClass: 'circles-text',
+			styleWrapper: true,
+			styleText: true
 		})
 
 		Circles.create({
-			id:'circles-3',
-			radius:45,
-			value:40,
-			maxValue:100,
-			width:7,
+			id: 'circles-3',
+			radius: 45,
+			value: 40,
+			maxValue: 100,
+			width: 7,
 			text: <?php echo  $total3; ?>,
-			colors:['#f1f1f1', '#F25961'],
-			duration:400,
-			wrpClass:'circles-wrp',
-			textClass:'circles-text',
-			styleWrapper:true,
-			styleText:true
+			colors: ['#f1f1f1', '#F25961'],
+			duration: 400,
+			wrpClass: 'circles-wrp',
+			textClass: 'circles-text',
+			styleWrapper: true,
+			styleText: true
 		})
 
 		var totalIncomeChart = document.getElementById('totalIncomeChart').getContext('2d');
@@ -601,7 +574,7 @@ if($query){
 			type: 'bar',
 			data: {
 				labels: ["S", "M", "T", "W", "T", "F", "S", "S", "M", "T"],
-				datasets : [{
+				datasets: [{
 					label: "Total Income",
 					backgroundColor: '#ff9e27',
 					borderColor: 'rgb(23, 125, 255)',
@@ -619,22 +592,22 @@ if($query){
 						ticks: {
 							display: false //this will remove only the label
 						},
-						gridLines : {
+						gridLines: {
 							drawBorder: false,
-							display : false
+							display: false
 						}
 					}],
-					xAxes : [ {
-						gridLines : {
+					xAxes: [{
+						gridLines: {
 							drawBorder: false,
-							display : false
+							display: false
 						}
 					}]
 				},
 			}
 		});
 
-		$('#lineChart').sparkline([105,103,123,100,95,105,115], {
+		$('#lineChart').sparkline([105, 103, 123, 100, 95, 105, 115], {
 			type: 'line',
 			height: '70',
 			width: '100%',
@@ -644,4 +617,5 @@ if($query){
 		});
 	</script>
 </body>
+
 </html>
